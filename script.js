96bda96a031a93b0ecca93952a21207f3aea503e
Vandesson Santiago <vandesson.santiago@outlook.com>
2023-01-31 14:50:04 +0000

    :tada: add dynamic favicon

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ function toggleMode(){
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
+  const favicon = document.querySelector("#favicon")
+
+  console.log(favicon)
 
   // substituir a img
   // se tiver light mode, pegar a tag img light
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
+    favicon.setAttribute("href", "./assets/favicon.ico")
   // se tiver dark mode, pegar a tag img dark
   } else {
     img.setAttribute("src", "./assets/avatar-dark.png")
+    favicon.setAttribute("href", "./assets/favicon-dark.ico")
   }
 }
 
